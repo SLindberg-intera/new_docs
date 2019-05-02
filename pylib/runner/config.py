e88ed6e9c4806d0fc11c9e587b1a2e86851fa54d
Kevin Smith <ksmith@intera.com>
2019-05-02 10:29:36 -0700

    inline docs

--- pylib/runner/config.py
+++ pylib/runner/config.py
@@ -1,16 +1,22 @@
 """
    configure the runner
 
-
    - reads the config file
-      - creates the command line arguments
+   - defines the command line arguments that are possible
+
+   
+   config is a dictionary containing the configuration parameters
+
+   parse_args is a function handle; 
+      when called, the command line arguments are parsed and returned
+      as a namespace
+   
 """
 import constants
 import sys, os
 
 sys.path.append(
         os.path.join(os.path.dirname(__file__), '..','..'))
-
 
 from pylib.config.config import read_config
 from pylib.autoparse.autoparse import config_parser 
