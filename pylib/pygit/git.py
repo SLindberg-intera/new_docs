115118ddbe3c1675a547bb9f04738f96ddaab516
Kevin Smith <ksmith@intera.com>
2019-05-02 14:15:37 -0700

    tools to validate branch

--- pylib/pygit/git.py
+++ pylib/pygit/git.py
@@ -8,6 +8,32 @@ import logging
 
 GIT_COMMAND = 'git'
 
+IS_NOT_A_GIT_COMMAND = 'is not a git command.'
+
+def check_output(out_bstr):
+    res = out_bstr.decode('utf-8')
+    if IS_NOT_A_GIT_COMMAND in res:
+        raise ValueError(res)
+    return res
+
+def get_version():
+    """ get the current tool version by inspecting git """
+    cmd = "log -1"
+    res = run_command(cmd)
+    key = res.split("commit")[1].split()[0]
+    return key
+
+def is_clean_master_branch():
+    """ True if we are on the last commit of the master branch
+    with nothing changed """
+    cmd = "status"
+    res = run_command(cmd)
+    if "On branch master" not in res:
+        return False
+
+
+    return res
+
 def run_command(command_str):
     """ runs a git command in the shell and returns the results as a text string"""
     shell_command = [GIT_COMMAND]
@@ -18,6 +44,7 @@ def run_command(command_str):
     out, err = proc.communicate()
     if err:
         raise IOError(e.decode("utf-8"))
-    return out.decode("utf-8")
+
+    return check_output(out) 
 
 
--- pylib/pygit/test_pygit.py
+++ pylib/pygit/test_pygit.py
@@ -1,8 +1,25 @@
 import unittest
+from . import git
 
 class TestFn_run_command(unittest.TestCase):
     def test_runs_ok(self):
-        self.assertTrue(False)
+        res = git.run_command("status")
+        self.assertTrue(res.startswith("On branch"))
+
+    def test_runs_junk(self):
+        with self.assertRaises(ValueError):
+            res = git.run_command("garbage fail")
+
+class TestFn_get_version(unittest.TestCase):
+    def test_executes(self):
+        res = git.get_version()
+        self.assertTrue(len(res)>0)
+
+class TestFn_is_clean_master_branch(unittest.TestCase):
+    def test_execute(self):
+        res = git.is_clean_master_branch()
+
+        print(res)
 
 if __name__ == "__main__":
     unittest.test()
