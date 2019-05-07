4252f6e716aaef09a38f2b27107cd942b1314661
Kevin Smith <ksmith@intera.com>
2019-05-07 11:54:55 -0700

    docs

--- pylib/vzreducer/config.py
+++ pylib/vzreducer/config.py
@@ -14,4 +14,6 @@ from pylib.config.config import read_config
 from pylib.autoparse.autoparse import config_parser
 
 config = read_config(constants.CONFIG_FILE)
+""" contains the results of the config file """
 parse_args = lambda: config_parser(config)
+""" reads parses the command line args and loads them to a namespace"""
