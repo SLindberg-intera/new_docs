ccb20906a516cc895589ce6dadf8d7498bb2876f
NPowers-intera <NPowers@intera.com>
2019-08-23 06:12:47 -0700

    updated example config files for hssmbuilder

--- pylib/hssmbuilder/example_config/run_hssm_tc-99.bat
+++ pylib/hssmbuilder/example_config/run_hssm_tc-99.bat
@@ -1,1 +1,1 @@
-py ../build_hssm_0.1.py config_tc-99.json
+py ..\..\ca-cie-tools\CA-CIE-Tools\pylib\runner\runner.py "py" "..\..\ca-cie-tools\CA-CIE-Tools\pylib\hssmbuilder\build_hssm_0.1.py config_tc-99.json" --gitpath "..\..\CA-CIE-tools\CA-CIE-Tools\.git" --logfile "cn_hssm_pkg.txt"
