7f3e7fbd3d9bc8ca24313cba1c4c1077c9c87883
Kevin Smith <ksmith@intera.com>
2019-10-28 15:04:36 -0700

    backbone; block chain parser

--- pylib/backbone/backbone.py
+++ pylib/backbone/backbone.py
@@ -9,9 +9,29 @@ import json
 HASH = 'Hash'
 INHERITANCE = 'Inheritance'
 
+class Connection:
+    """
+        a connection between two nodes.  Useful in building
+        a force diagram in D3
+    """
+    def __init__(self, source, target, value=1):
+        self.source = source
+        self.target = target
+        self.value = value
+
+    def __str__(self):
+        return "Connection(source={}, target={}, value={})".format(
+                self.source, self.target, self.value)
+    
+    def as_dict():
+        return {
+          "source":self.source, 
+          "target":self.target, "value":self.value}
+
 class Block:
     """ represents a block in the blockchain """
-    def __init__(self, hashkey, inheritance=[]):
+    def __init__(self, path, hashkey, inheritance=[]):
+        self.path = path
         self.hashkey = hashkey 
         self.inheritance = inheritance 
 
@@ -24,28 +44,44 @@ class Block:
             for path in d[INHERITANCE]:
                 p.append(cls.from_path(path))
 
-            return cls(d[HASH], p)
+            return cls(filepath, d[HASH], p)
 
-    def itr_nodes(self,):
-        """returns an iterator over nodes"""
-        yield self.hashkey
+    def __itr_nodes(self,):
+        """returns an iterator over nodes
+        
+        note nodes is not unique because there may be
+        multiple references to a node in the chain.
+
+        use self.nodes()  to get a unique list of nodes instead
+        """
+        yield self.path
         for node in self.inheritance:
-            yield node.hashkey
+            yield from node.__itr_nodes()
 
-    def itr_connections(self, ):
+    @property        
+    def nodes(self):
+        """ obtain a list of all the [unique] nodes in the blockchain """
+        return list(set(self.__itr_nodes()))
+
+    def __itr_connections(self, ):
         """ returns an iterator over connections"""
         for node in self.inheritance:
-            yield self.hashkey, node.hashkey
-            yield from node.itr_connections()
+            yield Connection(source=self.path, target=node.path)
+            yield from node.__itr_connections()
+
+    @property        
+    def connections(self):
+        """ obtain a list of all the connections in the blockchain """
+        return list(self.__itr_connections())
 
     def __str__(self):
         if(len(self.inheritance)>0):
-            return "[{}:{}]".format(self.hashkey,
+            return "[{}:{}]".format(self.path,
                     " ".join(map(str, self.inheritance)))
-        return "[{}]".format(self.hashkey)    
+        return "[{}]".format(self.path)    
 
 if __name__ == "__main__":
     blockpath = sys.argv[1]
     block = Block.from_path(blockpath)
-    #print(list(block.itr_nodes()))
-    print(list(block.itr_connections()))
+    print("Nodes", block.nodes)
+    print("Connections", ", ".join(map(str, block.connections)))
