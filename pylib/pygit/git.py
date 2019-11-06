4567143001a4ed64609a65c75e714988914cd7c4
Sara Lindberg <slindberg@intera.com>
2019-11-05 21:03:39 -0800

    updated runner.py and git.py from testing with debug input code

--- pylib/pygit/git.py
+++ pylib/pygit/git.py
@@ -24,6 +24,9 @@ def get_remote_master_version(path):
         NOTE: could be modified further to pass the branch in as an argument to make it more generic--the command
         doesn't necesarily have to be a "remote branch...string returned has a hard return so only returning the #[0:40]"
     """
+    cmd = "fetch"
+    #run git fetch to fetch most recent remote version (this is not a pull--does not affect branches)
+    run_command(cmd)
     cmd = "--git-dir={} rev-parse origin/master".format(path)
 
     return run_command(cmd)[0:40]
@@ -41,33 +44,44 @@ def get_version(path):
 def is_clean_master_branch(path):
     """ True if we are on the last commit of the master branch
     with nothing changed """
-
-    cmd = "status"
+    input('in the is clean master branch function')
+    cmd = "--work-tree={} status".format(path[0:-5])
     cmd = make_cmd(path,cmd)
-
+    #cmd = '-C {} --work-tree={} status'.format(path,path[0:-4])
+    input(cmd)
     res = run_command(cmd)
+    input(res)
     if "On branch master" not in res:
+        input('in the first if statement and returning false from there')
         return False
-
-    if "nothing to commit, working tree clean" in res:
+    
+    #version 1.9.1 =  nothing to commit, working directory clean
+    #version 2.21.0.windows.1 = nothing to commit, working tree clean
+    #update this conditional statement when git version on olive is updated
+    if "nothing to commit, working " and " clean" in res:
+        input('in the second if statement and returning true')
         return True
+    input('passed both if statements and returning last false')
     return False 
 
 
 def get_branch():
     """ returns the current branch name"""
     cmd = "branch"
+    input('getting the branch name....'.format(run_command(cmd)))
     res = run_command(cmd).split()[2]
+    input('the result is {}'.format(res))
     return res
 
 def make_cmd(path, cmd):
 
-    return "--git-dir {} {}".format(path,cmd)
+    return "--git-dir={} {}".format(path,cmd)
 
 def run_command(command_str):
     """ runs a git command in the shell and returns the results as a text string"""
     shell_command = [GIT_COMMAND]
     shell_command += command_str.split(" ")
+    input(shell_command)
     proc = subprocess.Popen(shell_command, 
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT)
--- pylib/runner/runner.py
+++ pylib/runner/runner.py
@@ -3,7 +3,7 @@ import sys, os
 from config import config, parse_args
 import logging
 from pylib.info.info import Info
-from pylib.pygit.git import get_version, is_clean_master_branch, get_remote_master_version 
+from pylib.pygit.git import get_version, is_clean_master_branch, get_remote_master_version , get_branch
 from pathlib import Path
 
 import subprocess
@@ -65,7 +65,7 @@ def make_user_summary():
             uname=info.uname,
             platform=info.platform,
             username=info.username
-    )
+    ) 
 
 def get_approved_tools():
     return config[c.APPROVED_TOOL_KEY]
@@ -123,32 +123,47 @@ def is_same_version(gitpath):
     return get_version(gitpath) == get_remote_master_version(gitpath)
 
 
-def is_on_qualified_list(args):
-    "determine which tool being invoked by the runner (if .exe (or ?) then = tool_name; if script then = 1st argument)"
+#def is_on_qualified_list(args):
+def is_on_qualified_list(tool_command):
+#determine which tool being invoked by the runner (if .exe (or ?) then = tool_name; if script then = 1st argument)
 # the following assumes that the command line statement is as follows:
 # .. python ..\runner.py 'perl|python|*.exe' "arguments [if perl or python, script first argument is tool script filename...]"
-    command = get_invoked_tool_name(args)
+    #command = get_invoked_tool_name(args)
 
 # this conditional statement may need to be updated depending upon how the CAST tool is called by the tool runner
-    if command[-4:] == '.exe':
-        tool_command = get_filename(command)
-    if command =='java':
-        tool_command = "PENDING--not sure what to do with CAST...java library and a .jar file"
-    else:
-        path_tool = get_invoked_tool_arguments(args).split(' ')[0]
-        tool_command = get_filename(path_tool)
+    #if command[-4:] == '.exe':
+    #    tool_command = get_filename(command)
+    #if command =='java':
+    #    tool_command = "PENDING--not sure what to do with CAST...java library and a .jar file"
+    #else:
+    #    path_tool = get_invoked_tool_arguments(args).split(' ')[0]
+    #    tool_command = get_filename(path_tool)
 
     approved_tools = get_approved_tools()
     for tool in approved_tools:
+        input('{} :{}'.format(tool_command,tool))
         if tool_command == tool['command']:
+            input(tool_command)
+            input(tool['command'])
             return True
     return False
 
-
+#note: args is a artifact--not currently used in the code for checking qa status
 def make_qa_status(args, tool, path):
     """ construct a string showing the QA Status"""
-
-    if path and is_same_version(path) and is_clean_master_branch(path) and is_on_qualified_list(args) :
+    
+    #DEBUG
+    input(get_branch())
+    input('{} : {}'.format(tool,path))
+    input('checking to see if {} is in a repository {}'.format(tool, path))
+    if path:
+        input ('checking to see if {} is in a clean master branch {}'.format(tool,is_clean_master_branch(path)))
+    input('checking to see if {} is is on the qualified tool list {}'.format(tool,is_on_qualified_list(tool)))
+    if path:
+        input('checking to see if local {} is same version as remote {}'.format(get_version(path), get_remote_master_version(path)))
+    #END DEBUG    
+    
+    if path and is_same_version(path) and is_clean_master_branch(path) and is_on_qualified_list(tool) and is_same_version(path):
         status = c.QA_QUALIFIED
     else:
         status = c.QA_TEST
@@ -169,12 +184,13 @@ def log_header(args,tg_dict):
     notify_user(make_user_message(args), shell=True)
     notify_user(make_tool_use_message(args))
     
-    #check versioning and QA status of both the runner and the tool(s) being invoked....
+    #check versioning (first for loop) and QA status (second for loop) of both the runner and the tool(s) being invoked....
     for tool, gitpath in tg_dict.items():
         notify_user(make_version(tool,gitpath))
 
     for tool, gitpath in tg_dict.items():
-        notify_user(make_qa_status(args,tool, gitpath))
+        #note: args is a artifact--not currently used in the code for checking qa status
+        notify_user(make_qa_status(args,get_filename(tool), gitpath))
     notify_user(make_user_summary())
 
 def execute_program(args):
@@ -184,13 +200,13 @@ def execute_program(args):
 
 if __name__ == "__main__":
     toolgitdict = {}
-    #add toolrunner to tool/gitpath dictionary
+    #add toolrunner filename and its gitpath to tool/gitpath dictionary
     toolgitdict[__file__] = get_gitpath(__file__)
 
      
     args = parse_args()
 
-    #get the tool(s) being invoked by runner and add to tool/gitpath dictionary--CAST references a library and jar file in repository
+    #get the tool(s) being invoked by runner and add to tool/gitpath dictionary--CAST references a library and jar file in repository--not handled at this time
     tool_list = get_pathtools(args)
     for path_tool in tool_list:
         toolgitdict[path_tool] =  get_gitpath(path_tool)
