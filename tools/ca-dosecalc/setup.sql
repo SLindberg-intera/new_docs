83aaec900f154d6488dd4d1a12ec7a2f8e7afc12
ksmith-intera <ksmith@intera.com>
2020-06-30 21:57:28 +0000

    database tables, functions, and sequences

---
+++ tools/ca-dosecalc/setup.sql
@@ -0,0 +1,4 @@
+create extension plpython3u;
+create extension postgis;
+create extension postgis_topology;
+create extension citext;
--- tools/ca-dosecalc/setupdb.sh
+++ tools/ca-dosecalc/setupdb.sh
@@ -6,18 +6,65 @@
 # 
 #   assumes user is a [postgres] superuser and can
 #   create/destroy databases and install extensions
+echo "START setupdb"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 db=$1 rep=$2
 dropdb $db
 createdb $db
-psql $db -f $DIR/setup.sql > $rep
-psql $db -f $DIR/model_seq.sql >> $rep
-#psql $db -f models.sql >> $rep
-#psql $db -f copc.sql >> $rep
-#psql $db -f pathways.sql >> $rep
-#psql $db -f cells.sql >> $rep
-#psql $db -f soils.sql >> $rep
-#psql $db -f cell_soils.sql >> $rep
-#psql $db -f concentrations.sql >> $rep
+psql $db -f $DIR/setup.sql >> $rep
+echo "Created Postgres Database with PostGIS extensions"
+#psql $db -f $DIR/setup.sql > $rep
+#    Sequences (used as primary keys) 
+psql $db -f $DIR/Sequences/model_mdl_id_seq.sql >> $rep
+psql $db -f $DIR/Sequences/concentration_bucket_bkt_id_seq.sql >> $rep
+psql $db -f $DIR/Sequences/grid_grid_id_seq.sql >> $rep
+psql $db -f $DIR/Sequences/map_shape_shp_id_seq.sql >> $rep
+psql $db -f $DIR/Sequences/map_shapes_properties_prp_id_seq.sql >> $rep
+psql $db -f $DIR/Sequences/map_image_img_id_seq.sql >> $rep
+psql $db -f $DIR/Sequences/shapefiles_shp_file_id_seq.sql >> $rep
+psql $db -f $DIR/Sequences/soil_type_soil_id_seq.sql >> $rep
+psql $db -f $DIR/Sequences/stg_hds_rid_seq.sql >> $rep
+psql $db -f $DIR/Sequences/stg_ucn_rid_seq.sql >> $rep
+psql $db -f $DIR/Sequences/stomp2gw_graphs_rid_seq.sql >> $rep
+psql $db -f $DIR/Sequences/stomp2gw_rid_seq.sql >> $rep
+psql $db -f $DIR/Sequences/units_unit_id_seq.sql >> $rep
+echo "Created Sequences" 
 
+#    Tables
+psql $db -f $DIR/Tables/models.sql >> $rep
+psql $db -f $DIR/Tables/copc.sql >> $rep
+psql $db -f $DIR/Tables/pathways.sql >> $rep
+psql $db -f $DIR/Tables/buckets.sql >> $rep
+psql $db -f $DIR/Tables/units.sql >> $rep
+psql $db -f $DIR/Tables/cells.sql >> $rep
+psql $db -f $DIR/Tables/soil_types.sql >> $rep
+psql $db -f $DIR/Tables/dose_factors.sql >> $rep
+psql $db -f $DIR/Tables/cell_heads.sql >> $rep
+psql $db -f $DIR/Tables/cell_soils.sql >> $rep
+psql $db -f $DIR/Tables/concentrations.sql >> $rep
+psql $db -f $DIR/Tables/shapefiles.sql >> $rep
+psql $db -f $DIR/Tables/model_shapefiles.sql >> $rep
+psql $db -f $DIR/Tables/map_shapes.sql >> $rep
+psql $db -f $DIR/Tables/map_shapes_attributes.sql >> $rep
+psql $db -f $DIR/Tables/map_images.sql >> $rep
+psql $db -f $DIR/Tables/stg_cells.sql >> $rep
+psql $db -f $DIR/Tables/stg_hds.sql >> $rep
+psql $db -f $DIR/Tables/stg_soils.sql >> $rep
+psql $db -f $DIR/Tables/stg_ucn.sql >> $rep
+psql $db -f $DIR/Tables/stomp2gw_graphs.sql >> $rep
+psql $db -f $DIR/Tables/stomp2gw_lookup.sql >> $rep
 
+#    Functions
+echo "END  setupdb"
+psql $db -f $DIR/Functions/icf_ucn_read.sql >> $rep
+psql $db -f $DIR/Functions/icf_ucn_read2.sql >> $rep
+psql $db -f $DIR/Functions/icf_util_copy_model.sql >> $rep
+psql $db -f $DIR/Functions/icf_util_get_srid.sql >> $rep
+psql $db -f $DIR/Functions/icf_write_file.sql >> $rep
+psql $db -f $DIR/Functions/'icf_util_map_shapes_attributes_load(integer).sql' >> $rep
+psql $db -f $DIR/Functions/icf_util_map_shapes_attributes_load.sql >> $rep
+psql $db -f $DIR/Functions/'icf_util_map_shapes_load(integer).sql' >> $rep
+psql $db -f $DIR/Functions/icf_util_map_shapes_load.sql >> $rep
+psql $db -f $DIR/Functions/icf_util_shp_to_mview.sql >> $rep
+psql $db -f $DIR/Functions/icf_ucn_get.sql >> $rep
+psql $db -f $DIR/Functions/icf_ucn_load.sql >> $rep
