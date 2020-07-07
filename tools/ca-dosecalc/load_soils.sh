126663997d34520b8a33399c591bfec7acbddd5f
ksmith-intera <ksmith@intera.com>
2020-07-07 21:22:36 +0000

    loading soils table

--- tools/ca-dosecalc/load_soils.sh
+++ tools/ca-dosecalc/load_soils.sh
@@ -12,11 +12,12 @@ where :
 "
 # Get command line arguments
 tkeep=1
-while getopts 'h:d:m:' opt; do
+while getopts 'h:f:d:m:' opt; do
   case "${opt}" in
     h)  echo "$usage" >&2
         exit;;
     m)  model=$OPTARG;;
+    f)  fpath=$OPTARG;;
     d)  dbase=$OPTARG;;
     :)  printf "missing argument for -%s\n" "$OPTARG" >&2
         echo "$usage" >&2
@@ -37,3 +38,21 @@ if [[ $mdlid == -1 ]]; then
   exit 1
 fi
 
+# drop staging table
+res=$(psql -d "$dbase" -qtA -c "drop table if exists public.stg_soils;")
+
+# create staging table
+res=$(psql -d "$dbase" -qtA -c "create table stg_soils ("ID" character varying(80), "ROW" integer,"COL" integer, "SOIL_CATEGORY" character varying(90),"SOIL_INDEX" integer);")
+
+# load data into staging
+res=$(psql -d "$dbase" -qtA -c "copy stg_soils from '$fpath' csv header delimiter ',';")
+
+# populate the soil_types table
+res=$(psql -d "$dbase" -qtA -c "delete from soil_types where mdl_id=$mdlid;")
+res=$(psql -d "$dbase" -qtA -c "insert into soil_types (soil_desc, soil_nm, mdl_id, soil_id) (select distinct soil_category as soil_desc, left(soil_category, 30) as soil_nm, $mdlid as mdl_id, soil_index as soil_id from stg_soils);")
+
+# populate cell_soils
+res=$(psql -d "$dbase" -qtA -c "insert into cell_soils (cell_id, soil_id) (select cell_id, soil_id from stg_soils S, soil_types T, cells C where C.row=S.row and C.col=S.col and T.mdl_id=$mdlid and C.mdl_id=$mdlid and T.soil_id=S.soil_index);")
+
+# drop staging table
+res=$(psql -d "$dbase" -qtA -c "drop table stg_soils;")
