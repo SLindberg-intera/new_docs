cf17e2da6137b3c78279aa916ecb3962e467cbe6
ksmith-intera <ksmith@intera.com>
2020-07-22 21:29:09 +0000

    bug: precision in dose factor output?

--- tools/ca-dosecalc/output_dose.sh
+++ tools/ca-dosecalc/output_dose.sh
@@ -6,6 +6,6 @@ fout=$4
 mdlid=1
 d=$(date)
 echo "$d: START Export of dose data for '$copc' to '$fout' "
-res=$(psql -d "$dbase" -qtA -c "copy (select elapsed_tm,  model_date::date, soil, pathway, cell_row, cell_column, cell_layer, to_char(concentration, '$dFormat') as concentration, dose_factor, to_char(dose, '$dFormat') as dose from mv_dose where mdl_id=$mdlid and copc='$copc' order by elapsed_tm, pathway, cell_row, cell_column, cell_layer ) to '$fout' delimiter ',' csv header;")
+res=$(psql -d "$dbase" -qtA -c "copy (select elapsed_tm,  model_date::date, soil, pathway, cell_row, cell_column, cell_layer, to_char(concentration, '$dFormat') as concentration, to_char(dose_factor, '$dFormat'), to_char(dose, '$dFormat') as dose from mv_dose where mdl_id=$mdlid and copc='$copc' order by elapsed_tm, pathway, cell_row, cell_column, cell_layer ) to '$fout' delimiter ',' csv header;")
 d=$(date)
 echo "$d: END Export of dose data."
